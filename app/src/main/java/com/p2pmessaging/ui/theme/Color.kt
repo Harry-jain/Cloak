dca3e9a6da23753bd50c9ea8d6f36121fe53a046
Harsh jain <109072049+Harry-jain@users.noreply.github.com>
2025-09-15 00:36:39 +0530

    Add Color.kt

---
+++ app/src/main/java/com/p2pmessaging/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.p2pmessaging.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom colors for the app
+val RadarBackground = Color(0xFF121212)
+val RadarCircle = Color(0xFF404040)
+val PeerDot = Color(0xFF00FF00)
+val MessageSent = Color(0xFF2196F3)
+val MessageReceived = Color(0xFF424242)
